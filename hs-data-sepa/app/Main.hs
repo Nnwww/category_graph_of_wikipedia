08d997c3bd1b9d4fe25cfb88b238bd6df25cf8d4
Nnwww <johndororo@gmail.com>
2018-02-07 02:37:58 +0900

    delete cassava

--- hs-data-sepa/app/Main.hs
+++ hs-data-sepa/app/Main.hs
@@ -1,105 +1,147 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE StrictData #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE StrictData        #-}
+{-# LANGUAGE TypeApplications  #-}
 module Main where
-import           Database.Redis
-import qualified Data.Vector as V
-import           Data.Text    (Text, split)
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
-import           GHC.Generics (Generic)
-import           GHC.Conc (numCapabilities)
-import           Data.Csv
-import           Data.Char (ord)
-import           Data.Either
-import           Data.Monoid
-import           Data.Ratio
-import           System.Exit
-import           Data.Conduit
-import qualified Data.Conduit.Binary as CB
-import qualified Data.Conduit.Combinators as CC
 import           Control.Concurrent.Async
-import           Control.Monad.Trans.Resource
+import           Control.Exception.Safe
 import           Control.Monad.IO.Class
 import           Control.Monad.State
-import           Control.Exception.Safe
-import           System.IO (hFileSize, withFile, IOMode(ReadMode))
-import           System.Environment (getArgs)
+import           Control.Monad.Trans.Resource
+import qualified Data.ByteString              as B
+import qualified Data.ByteString.Lazy         as BL
+import           Data.Char                    (ord)
+import           Data.Conduit
+import qualified Data.Conduit.Binary          as CB
+import qualified Data.Conduit.Combinators     as CC
+import qualified Data.Conduit.Text            as CT
+import           Data.Either
+import           Data.Maybe
+import           Data.Monoid
+import           Data.Ratio
+import           Data.String
+import qualified Data.Text                    as T
+import qualified Data.Text.Encoding           as TE
+import qualified Data.Vector                  as V
+import           Database.Redis
+import           GHC.Conc                     (numCapabilities)
+import           GHC.Generics                 (Generic)
+import           GHC.Stack.Types              (CallStack, HasCallStack,
+                                               getCallStack)
+import           System.Environment           (getArgs)
+import           System.Exit
+import           System.IO                    (IOMode (ReadMode), hFileSize,
+                                               withFile)
 
 data WikiEnTSV = WikiEnTSV
   { entryID            :: Int
   , namespaceID        :: Int
-  , entry              :: B.ByteString
-  , hiragana           :: B.ByteString
-  , author             :: B.ByteString
-  , createdDate        :: B.ByteString
-  , lastestUpdatedDate :: B.ByteString
+  , entry              :: T.Text
+  , ailias             :: T.Text
+  , author             :: T.Text
+  , createdDate        :: T.Text
+  , lastestUpdatedDate :: T.Text
   , numOfEdit          :: Int
-  , category           :: B.ByteString
-  , redirect1          :: B.ByteString
-  } deriving (Generic, Show)
-
-instance FromRecord WikiEnTSV
-instance ToRecord WikiEnTSV
+  , category           :: T.Text
+  , redirect1          :: T.Text
+  } deriving (Show)
 
 articleKeyHeader = (<>) "article:"
-separateTab = defaultDecodeOptions {decDelimiter = fromIntegral (ord '\t')}
 
 type YDataMonad = StateT YDataState IO
 type YDataState = Int
 
 collectArticlesToRedis :: Connection -> WikiEnTSV -> YDataMonad ()
 collectArticlesToRedis conn tsvLine = do
-  let categories = B.split (fromIntegral . ord $ ';') . category $ tsvLine
-      addRelationOfCategoryToArticle = (flip sadd) ([entry tsvLine]) . articleKeyHeader
+  let categories = T.splitOn ";" . category $ tsvLine
+      addRelationOfCategoryToArticle = flip sadd [TE.encodeUtf8 $ entry tsvLine]
+        . articleKeyHeader . TE.encodeUtf8
   liftIO . runRedis conn $ mapM_ addRelationOfCategoryToArticle categories
 
 gigaByte = 1024 ^ 3
 handleRange h offset segSize bufferSize =
   CB.sourceHandleRangeWithBuffer h (Just offset) (Just segSize) bufferSize
 
+lineProcess :: Int -> IO ()
+lineProcess numProcess =
+  withFile "wikipedia-en.txt" ReadMode $ \h -> do
+  fileSize <- hFileSize h
+  let invCap = 1 / realToFrac numProcess
+  let readSizeOfSegments = fileSize `div` toInteger numProcess
+  mapConcurrently_ seqYData
+    . map (\ratioOffset -> handleRange h (floor $ ratioOffset * realToFrac fileSize) readSizeOfSegments gigaByte)
+    . take numProcess $ enumFromThenTo @Double 0 invCap 1
+
+seqYData :: ConduitM () B.ByteString YDataMonad () -> IO ()
+seqYData yDataSeqqer = do
+  conn <- checkedConnect defaultConnectInfo
+  flip evalStateT 0 . runConduit
+    $  yDataSeqqer
+    .| CC.decodeUtf8
+    .| CT.lines
+    .| decodeToWikiEnTSV
+    .| rightOrDie
+    .| CC.filter ((== 0) . namespaceID) -- 0 is article. see https://en.wikipedia.org/wiki/Wikipedia:Namespace
+    .| CC.mapM_ (collectArticlesToRedis conn)
+
+last' :: T.Text -> Maybe Char
+last' t | T.length t > 0 = Just $ T.last t
+last' _ = Nothing
+
+parseWikiEnTSV :: T.Text -> Either SomeException WikiEnTSV
+parseWikiEnTSV lineMaybeWithNewLine =
+  case (== '\n') <$> last' lineMaybeWithNewLine of
+    Just True  -> splitTSV . fst . fromJust . T.unsnoc $ lineMaybeWithNewLine
+    Just False -> splitTSV lineMaybeWithNewLine
+    Nothing    -> throwString "Parse Error: input is an empty."
+  where
+    splitTSV line =
+      let texts = T.splitOn "\t" line
+          numTexts = length texts in
+        if numTexts /= 10 then
+          throwString $
+          "Parse Error: too " <>
+          (if numTexts > 10 then "many" else "few") <>
+          " field in a record.\n" <>
+          "input record: " <> show texts
+        else
+          Right WikiEnTSV { entryID            = read . T.unpack $ texts !! 0
+                          , namespaceID        = read . T.unpack $ texts !! 1
+                          , entry              = texts !! 2
+                          , ailias             = texts !! 3
+                          , author             = texts !! 4
+                          , createdDate        = texts !! 5
+                          , lastestUpdatedDate = texts !! 6
+                          , numOfEdit          = read . T.unpack $ texts !! 7
+                          , category           = texts !! 8
+                          , redirect1          = texts !! 9
+                          }
+
+awaitJust :: Monad m => (i -> ConduitM i o m ()) -> ConduitM i o m ()
+awaitJust f =
+  await >>= \case
+    Nothing -> pure ()
+    Just x -> f x
+
+decodeToWikiEnTSV :: (MonadThrow m, MonadState YDataState m)
+  => ConduitM T.Text (Either SomeException WikiEnTSV) m ()
+decodeToWikiEnTSV = do
+  lift $ modify' (+1)
+  awaitJust $ \line -> if T.null line
+    then pure ()
+    else yield $ parseWikiEnTSV line
+
+rightOrDie :: (MonadThrow m, MonadState YDataState m, MonadIO m)
+  => ConduitM (Either SomeException WikiEnTSV) WikiEnTSV m ()
+rightOrDie =
+  awaitJust $ \case
+  Right l -> yield l
+  Left err -> do
+    lineNum <- Control.Monad.State.get
+    throwString $ "parse error at line " <> show lineNum <> ": " <> show err
+
 main :: IO ()
 main = do
   args <- getArgs
   lineProcess $ if length args == 1 then read @Int $ head args else numCapabilities
-  where
-    lineProcess :: Int -> IO ()
-    lineProcess numProcess =
-      let invCap = 1 / (realToFrac numProcess) in
-        withFile "wikipedia-en.txt" ReadMode $ \h -> do
-          fileSize <- hFileSize h
-          let readSizeOfSegments = fileSize `div` (toInteger numProcess)
-          mapConcurrently_ seqYData
-            . map (\ratioOffset -> handleRange h (floor $ ratioOffset * (realToFrac fileSize)) readSizeOfSegments gigaByte)
-            . take numProcess $ enumFromThenTo @Double 0 invCap 1
-    seqYData :: ConduitM () B.ByteString YDataMonad () -> IO ()
-    seqYData yDataSeqqer = do
-      conn <- checkedConnect defaultConnectInfo
-      (flip evalStateT 0) . runConduit
-        $  yDataSeqqer
-        .| CB.lines
-        .| CC.mapM decodeToWikiEnTSV
-        .| rightOrDie
-        .| CC.filter ((== 0) . namespaceID) -- 0 is article. see https://en.wikipedia.org/wiki/Wikipedia:Namespace
-        .| CC.mapM_ (collectArticlesToRedis conn)
-    decodeToWikiEnTSV :: B.ByteString -> YDataMonad (Either String (V.Vector WikiEnTSV))
-    decodeToWikiEnTSV line = do
-      modify' (+1)
-      pure . decodeWith separateTab NoHeader $ BL.fromStrict line
-    rightOrDie :: (MonadThrow m, MonadState YDataState m, MonadIO m)
-      => ConduitM (Either String (V.Vector WikiEnTSV)) WikiEnTSV m ()
-    rightOrDie =
-      await >>= \case
-      Nothing -> pure ()
-      Just x  -> case x of
-                   Right l | V.length l == 1 -> yield . V.head $ l
-                   Left err -> do
-                     lineNum <- Control.Monad.State.get
-                     throwString $ "parse error at line " <> show lineNum <> ": " <> err
-                   Right l -> do
-                     liftIO . putStrLn $
-                       "Detect more than one parse by cassava at a time. check following contents: " <> show l
-                     CC.yieldMany l
